7abc019b7d52369a377f7f5608973b11c8b1b7db
Subhash Chandra <TMaYaD@LoonyB.in>
2013-11-26 02:12:34 +0530

    added clients and configured s3

--- config.rb
+++ config.rb
@@ -60,13 +60,16 @@ activate :livereload
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  # Minify Html on build
+  activate :minify_html
 
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
 
   # Use relative URLs
   activate :relative_assets
@@ -74,8 +77,32 @@ configure :build do
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
-  # activate :smusher
+  activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
+
+  activate :gzip
+
+  activate :favicon_maker,
+            favicon_maker_base_image: "images/favicon.png"
 end
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                = 'www.loonyb.in'
+  s3_sync.region                = 'us-east-1'
+  s3_sync.aws_access_key_id     = ENV['AWS_KEY']
+  s3_sync.aws_secret_access_key = ENV['AWS_SECRET']
+  s3_sync.delete                = true
+  s3_sync.after_build           = false
+  s3_sync.prefer_gzip           = true
+end
+
+# activate :cloudfront do |cloudfront|
+#   cloudfront.access_key_id     = ENV['AWS_KEY']
+#   cloudfront.secret_access_key = ENV['AWS_SECRET']
+#   cloudfront.distribution_id   = 'E3E0TLTQTS8L1F'
+#   cloudfront.after_build       = false
+# end
+
+
